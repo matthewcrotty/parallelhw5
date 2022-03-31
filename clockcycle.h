2e6be584e1c2426f9fc1f504b66735566507b4ac
Crotty <PCPBcrtt@dcsfen01.ccni.rpi.edu>
2022-03-31 11:08:07 -0400

    final scripts

--- clockcycle.h
+++ clockcycle.h
@@ -3,21 +3,7 @@
 
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
 
-#ifdef __clang__
-static inline uint64_t clock_now() {
-	return __builtin_readcyclecounter();
-}
-#elif defined(__GNUC__)
-#if defined(__i386__) || defined(__x86_64__) || defined(__amd64__)
-#include <x86intrin.h>
-static inline uint64_t clock_now() {
-	return __rdtsc();
-}
-#else
 uint64_t clock_now(void)
 {
   unsigned int tbl, tbu0, tbu1;
@@ -30,10 +16,5 @@ uint64_t clock_now(void)
 
   return (((uint64_t)tbu0) << 32) | tbl;
 }
-#endif
-#endif
-#ifdef __cplusplus
-}
-#endif // __cplusplus
 
 #endif // CLOCKCYCLE_H
